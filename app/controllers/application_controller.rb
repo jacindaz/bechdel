47d0c7e6bf31a3cc9ec8ded4ed7c94384c8b5ef1
Jacinda Zhong <jacindaz@gmail.com>
2014-07-09 20:56:32 -0400

    Updates registration page, requires username
     - when signing up, requires you enter a username
     - when you are signed in, displays your username and
       says that you are signed in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,6 +9,5 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up) << [:username, :avatar_url, :provider, :location]
-    binding.pry
   end
 end
