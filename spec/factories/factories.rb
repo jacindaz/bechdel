5840e71b0dd9ea60792a9a83d149a367d953d379
Jacinda Zhong <jacindaz@gmail.com>
2014-07-10 11:45:24 -0400

    Test to sign in, create new movie, not working
     - not sure why???
     - working in browser

--- spec/factories/factories.rb
+++ spec/factories/factories.rb
@@ -45,4 +45,11 @@ FactoryGirl.define do
     vote -1
   end
 
+  factory :user do
+    sequence(:username) {|n| "filmbuff#{n}"}
+    sequence(:email) {|n| "jazz#{n}@movies.com"}
+    password '1234567890'
+    password_confirmation '1234567890'
+  end
+
 end
--- spec/features/movies/user_creates_new_movie.rb
+++ spec/features/movies/user_creates_new_movie.rb
@@ -2,28 +2,36 @@ require 'rails_helper'
 
 feature 'user creates a new movie' do
 
-  scenario 'user creates a new movie using a form' do
-    movie = FactoryGirl.create(:movie)
-    visit new_movie_path
+  context 'as an authenticated user' do
+    let(:user) { FactoryGirl.create(:user) }
 
-    fill_in "Title", with: movie.title
-    select movie.year, from: "Year"
-    fill_in "Summary", with: movie.summary
-    select movie.language, from: "Language"
-    select movie.country_produced, from: "Country"
-    click_on "Submit New Movie"
+    before :each do
+      sign_in_as(user)
+    end
 
-    expect(page).to have_content "Movie saved."
-    expect(page).to have_content movie.title
-    expect(page).to have_content movie.year
-    expect(page).to have_content movie.summary
+    scenario 'user creates a new movie using a form' do
+      movie = FactoryGirl.create(:movie)
+      visit new_movie_path
+
+      fill_in "title", with: movie.title
+      select movie.year, from: "Year"
+      fill_in "Summary", with: movie.summary
+      select movie.language, from: "Language"
+      select movie.country_produced, from: "Country"
+      click_on "Submit New Movie"
+
+      expect(page).to have_content "Movie saved."
+      expect(page).to have_content movie.title
+      expect(page).to have_content movie.year
+      expect(page).to have_content movie.summary
+    end
+
+    scenario 'user enters a blank movie' do
+      visit new_movie_path
+      click_on "Submit New Movie"
+
+      expect(page).to have_content "Movie not saved."
+    end
+
   end
-
-  scenario 'user enters a blank movie' do
-    visit new_movie_path
-    click_on "Submit New Movie"
-
-    expect(page).to have_content "Movie not saved."
-  end
-
 end
