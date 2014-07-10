18ec8108ad7194a7cb1bc0cdf4c14d647c8e324f
Jacinda Zhong <jacindaz@gmail.com>
2014-07-09 20:51:56 -0400

    Adds log in/out link on all pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,5 +9,6 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up) << [:username, :avatar_url, :provider, :location]
+    binding.pry
   end
 end
