80cf09189f15ab9a5cf9c380154d7460aa994414
Jacinda Zhong <jacindaz@gmail.com>
2014-07-17 11:39:14 -0400

    Working version of scraping for movie URLs
     - takes in the year in class initializer
     - generates URL for each Cannes selection year
     - selection_links method parses and finds URL of each movie
       that was selected in that year

--- lib/cannes_scraping.rb
+++ lib/cannes_scraping.rb
@@ -14,12 +14,30 @@ class CannesScraping
       individual_link = movie_link.attributes["href"].value
       movie_title = movie_link.text
       movie[:title] = movie_title
-      movie[:url] = individual_link
+      movie[:url] = "http://www.festival-cannes.fr#{individual_link}"
       movies << movie
     end
+    movies
   end
 
+  def movie_info
 
-#page.css('ul.list-movies-1').css('li').css('a')[0].attributes["href"].value
+  end
 
 end
+
+
+# create_table "movies", force: true do |t|
+#     t.string   "title",                                      null: false
+#     t.integer  "year",                                       null: false
+#     t.text     "summary",                                    null: false
+#     t.string   "language",                                   null: false
+#     t.string   "country_produced",                           null: false
+#     t.string   "bechdel_rating",   default: "no rating",     null: false
+#     t.integer  "user_id",                                    null: false
+#     t.datetime "created_at"
+#     t.datetime "updated_at"
+#     t.integer  "up_votes",         default: 0,               null: false
+#     t.integer  "down_votes",       default: 0,               null: false
+#     t.string   "thumbnail_url",    default: "no-image.jpeg"
+#   end
