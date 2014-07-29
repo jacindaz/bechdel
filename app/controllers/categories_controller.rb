fe782a868208aa1b9988760594e7a0ad5307c2a6
Jacinda Zhong <jacindaz@gmail.com>
2014-07-29 16:49:59 -0400

    Remove binding.pry

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -2,7 +2,8 @@ class CategoriesController < ApplicationController
 
   def index
     @categories = Category.where(category: params[:sort_by])
-    binding.pry
+    #binding.pry
+
     @title = Category.return_index_title(params[:sort_by])
   end
 
