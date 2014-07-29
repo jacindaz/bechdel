e7c522f6576986b3284f919e6a6bbe74c688d2c3
Jacinda Zhong <jacindaz@gmail.com>
2014-07-29 16:47:56 -0400

    Adds text if no categories, debugging heroku category box_office sorting

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -2,6 +2,7 @@ class CategoriesController < ApplicationController
 
   def index
     @categories = Category.where(category: params[:sort_by])
+    binding.pry
     @title = Category.return_index_title(params[:sort_by])
   end
 
