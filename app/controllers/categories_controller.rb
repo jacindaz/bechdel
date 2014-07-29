7bcf1dc90e3f8cf8c83f8be5f1806198ba49aabe
Jacinda Zhong <jacindaz@gmail.com>
2014-07-29 16:31:57 -0400

    Updates params[:sort_by], removes string interpoloation from Categories controller

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,7 +1,7 @@
 class CategoriesController < ApplicationController
 
   def index
-    @categories = Category.where(category: "#{params[:sort_by]}")
+    @categories = Category.where(category: params[:sort_by])
     @title = Category.return_index_title(params[:sort_by])
   end
 
