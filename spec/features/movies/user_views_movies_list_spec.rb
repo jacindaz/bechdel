446549cecba0bc3c3fc8fbb5a195c2219e06e21e
Jacinda Zhong <jacindaz@gmail.com>
2014-08-10 10:54:10 -0400

    Creates and passes feature test - user votes on movie

--- spec/features/movies/user_views_movies_list_spec.rb
+++ spec/features/movies/user_views_movies_list_spec.rb
@@ -16,7 +16,6 @@ feature 'user views a list of movies' do
       expect(page).to have_content movie.title
       expect(page).to have_content movie.year
       expect(page).to have_content movie.summary
-      expect(page).to have_content movie.language
       expect(page).to have_content movie.country_produced
     end
 
--- spec/features/movies/user_votes_on_one_movie_spec.rb
+++ spec/features/movies/user_votes_on_one_movie_spec.rb
@@ -2,11 +2,20 @@ require 'rails_helper'
 
 feature 'user votes on a particular movie' do
 
-  scenario 'user votes on a particular movie' do
+  context 'as an authenticated user' do
+    let(:user) { FactoryGirl.create(:user) }
+
+    before :each do
+      sign_in(user)
+    end
+
+    scenario 'user votes passes a particular movie' do
+      movie = FactoryGirl.create(:movie)
+      visit movie_path(movie)
+
+      click_on "Pass"
+      expect(page).to have_content "Thanks for already voting on this movie!"
+    end
 
   end
-
-  scenario 'user changes vote on a particular movie' do
-  end
-
 end
